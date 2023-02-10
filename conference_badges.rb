13b100b6de613fa0606255f60a4e5d4f823379ec
tugi <blueqwinn@gmail.com>
2023-02-10 21:08:42 +0300

    first commit

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+badge_maker("Kathy")
+# => ["Hello , my name is Kathy"]
+
+badge_Badge_creator(["Kathy", "Stacy"])
+# => ["Hello, my name is Kathy." , "Hello, my name is Stacy."]
+
+assign_rooms(["Kathy", "Stacy"])
+# => ["Hello, Kathy! You'll be assigned to room 1!", "Hello, Stacy! You'll be assigned to room 2!"]
+
+
+printer (["Kathy", "Stacy"])
+# Hello , my name is Kathy.
+# Hello, my name is Stacy
+# Hello, Kathy! You'll be assigned to room 1!
+# Hello, Stacy! You'll be assigned to room 2!
+
